bc3e5cb91141da9bc8696fdd17fb9c109dd2377e
Keerthana-R-Gowda <keerthanar0601@gmail.com>
2024-06-21 23:08:58 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,38 @@
 from bs4 import BeautifulSoup
+import requests
+import time
+print('put skill that you are not familiar with')
+unfamiliar_skill = input('>')
+print(f'Filtering out {unfamiliar_skill}')
 
-with open('home.html','r') as html_file:
-    content = html_file.read()
+def find_jobs():
+    html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation=').text
+    soup = BeautifulSoup(html_text, 'lxml')
+    jobs  = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+    for job in jobs:
+        published_date = job.find('span', class_ = 'sim-posted').span.text
+        if '3' in published_date:
+            company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace('','')
+            skills = job.find('span', class_ = 'srp-skills').text.replace('','')
+            more_info = job.header.h2.a['href']
+            if unfamiliar_skill not in skills:
+                print(f"Company Name: {company_name.strip()}")
+                print(f"Required Skills: {skills.strip()}")
+                print(f'More Info: {more_info}')
+
+                print('')
+
+if __name__ == '__main__':
+    while True:
+        find_jobs()
+        time_wait = 10
+        print(f'Waiting {time_wait} seconds...')
+        time.sleep(time_wait * 60)
     
-    soup = BeautifulSoup(content, 'lxml')
-    course_cards = soup.find_all('div', class_ = 'card')
-    for course in course_cards:
-        course_name = course.h5.text
-        course_price = course.a.text
 
-        print(f'{course_name} costs {course_price}')
+
+
+
         
         
 
